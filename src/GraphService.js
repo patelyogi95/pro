19014e6ebaeeff4b496d9742aca90992cad3e84e
Yogeshbhai Patel <YPatel@asginc.us>
2020-04-17 01:19:24 -0400

    1. Formatting changes. 2. Changes for welcoming first time user to signin immediately. 3. Handling user cancelling login flow.

--- src/GraphService.js
+++ src/GraphService.js
@@ -1,21 +1,21 @@
 var graph = require('@microsoft/microsoft-graph-client');
 
 function getAuthenticatedClient(accessToken) {
-  // Initialize Graph client
-  const client = graph.Client.init({
-    // Use the provided access token to authenticate
-    // requests
-    authProvider: (done) => {
-      done(null, accessToken.accessToken);
-    }
-  });
+    // Initialize Graph client
+    const client = graph.Client.init({
+        // Use the provided access token to authenticate
+        // requests
+        authProvider: (done) => {
+            done(null, accessToken.accessToken);
+        }
+    });
 
-  return client;
+    return client;
 }
 
 export async function getUserDetails(accessToken) {
-  const client = getAuthenticatedClient(accessToken);
+    const client = getAuthenticatedClient(accessToken);
 
-  const user = await client.api('/me').get();
-  return user;
+    const user = await client.api('/me').get();
+    return user;
 }
